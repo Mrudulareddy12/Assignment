f5769754b8e16e829089297b2bccf7a1030210f5
unknown <mrudulareddy1401@gmail.com>
2021-08-16 17:03:39 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment';
-}
+  myid:any;
+  myform:any;
+  ngOnInit()
+  {
+    this.myform=new FormGroup({
+    firstname:new FormControl(''),
+    lastname:new FormControl(''),
+    phonenumber:new FormControl(''),
+    city:new FormControl(''),
+    state:new FormControl(''),
+    pincode:new FormControl('')
+
+    });
+  
+    
+    this.display();
+  }
+  
+  
+  
+  display()
+  {
+    return localStorage.getItem('formdata');
+  }
+  onSubmit()
+  {
+    localStorage.setItem("formdata",JSON.stringify(this.myform.value))
+  }
+  removeItem()
+  {
+    localStorage.removeItem('formdata');
+  }
+  removeAll()
+  {
+    localStorage.clear();
+  }
+  update()
+  {
+    localStorage.setItem("formdata",JSON.stringify(this.myform.value)) 
+  }
+  
+  
+   
+  }
+  
+ 
+
